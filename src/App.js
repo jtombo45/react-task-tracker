df3e1a6b6d6844dff0e3f80cad98439cfe431154
jtombo45 <jtombo@asu.edu>
2022-03-24 18:51:25 -0700

    'initial commit: removed template files and the div content from App.js'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const name = 'Jude'
+  const x = false
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h1>Hello {name} </h1>
+      <h1>1 + 1 = {1 + 1}</h1>
+      <h1>Hello {x ? 'And Welcome Back' : 'And who am I talking to?'}</h1>
     </div>
   );
 }
 
 export default App;
+
