7535a6f0af3519322387b2fa4951539891f75aa8
jtombo45 <jtombo@asu.edu>
2022-03-25 10:18:02 -0700

    Inline styling with embedded css or JSX

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
   return (
     <header>
-        <h1>
+        <h1 style={{color: 'red', backgroundColor: 'black'}}>
             {title}
         </h1>
     </header>
   )
 }
 
-// Header.defaultProps = {
-//     title: 'Task Tracker',
-// }
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
