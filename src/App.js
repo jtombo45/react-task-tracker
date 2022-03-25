ce7fda2bc48a64b6887561643717e44be75db47e
jtombo45 <jtombo@asu.edu>
2022-03-24 22:05:25 -0700

    Working with props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 
+import React from 'react'
+import Header from "./components/Header";
 
 function App() {
-  const name = 'Jude'
-  const x = false
-  return (
-    <div className="container">
-
-      <h1>Hello {name} </h1>
-      <h1>1 + 1 = {1 + 1}</h1>
-      <h1>Hello {x ? 'And Welcome Back' : 'And who am I talking to?'}</h1>
-    </div>
-  );
+ const name = 'Jude'
+ const x = false
+ return (
+   <div className="container">
+     <Header title='Hello'/>
+   </div>
+ );
 }
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
 
 export default App;
 
