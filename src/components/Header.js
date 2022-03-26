fbf45e566a12b3dac94371e862bcb651373edcb1
jtombo45 <jtombo@asu.edu>
2022-03-25 17:23:19 -0700

    Added event function for button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({title}) => {
+  const onClick = () => {
+    console.log('Click')
+  }
   return (
     <header className='header'>
         <h1>
             {title}
         </h1>
-        <Button color='green' text='Add'/>
+        <Button color='green' text='Add' onClick={onClick}/>
         
     </header>
   )
@@ -20,6 +23,7 @@ Header.defaultProps = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
+    color: PropTypes.string
 }
 
 //const headStyle = {
